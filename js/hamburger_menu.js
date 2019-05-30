a5d9aa333586e4db94b6a2cc20605868f42e288a
Slava Karpachev <3151546@gmail>
2019-05-30 12:44:54 +0300

    моё задание бургер 12:44

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,7 +1,6 @@
 const burgerMenu = document.querySelector('.burger-menu');
-const nav = document.querySelector('.nav');
-const сompanyList = document.querySelector('.nav__list');
-const сompanyListLink = document.querySelectorAll('.nav__link');
+      nav = document.querySelector('.nav');
+      сompanyList = document.querySelector('.nav__list');
 
 burgerMenu.addEventListener('click', function() {
   burgerMenu.classList.toggle('burger-menu--active');
@@ -9,9 +8,3 @@ burgerMenu.addEventListener('click', function() {
   сompanyList.classList.toggle('nav__list-active');
   document.body.classList.toggle('body-overflow');
 });
-
-сompanyListLink.addEventListener('click', function() {
-  сompanyList.classList.remove('nav__list-active');
-  nav.classList.remove('nav__active');
-  document.body.classList.remove('body-overflow');
-})
